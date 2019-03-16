612acd7aef9398c7bc9bd92e334a92c3a2e78a97
David Given <dg@cowlark.com>
2019-03-16 12:41:40 +0100

    Rework to use LanguageService, which is simpler and faster.

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,152 +1,95 @@
 let ts = (window as any).ts;
 
-enum FileWatcherEventKind {
-    Created,
-    Changed,
-    Deleted
-}
-
-type FileWatcherCallback = (path: string, eventKind: FileWatcherEventKind) => void;
-
-class FileWatcher {
-    file: VirtualFile;
-    callback: FileWatcherCallback;
-
-    close(): void {
-        this.file.watchers.delete(this);
-    }
-}
-
 class VirtualFile {
     name: string;
     contents: string;
-    watchers = new Set<FileWatcher>();
+    version: number = 0;
 }
 
-let sys = new class System {
-    private files = new Map<String, VirtualFile>();
+let compilationOptions = {
+    target: ts.ScriptTarget.ES5,
+    strict: true,
+    suppressOutputPathCheck: false,
+    extendedDiagnostics: true,
+    noEmitHelpers: true,
+};
+
+let languageServiceHost = new class LanguageServiceHost {
+    private files = new Map<string, VirtualFile>();
+    private projectVersion = 0;
+
+    getScriptFileNames(): string[] {
+        return Array.from(this.files.keys());
+    }
+
+    getScriptVersion(path: string): number {
+        let file = this.files.get(path);
+        return (file == undefined) ? undefined : file.version;
+    }
+
+    getProjectVersion(): number {
+        return this.projectVersion;
+    }
+
+    getScriptSnapshot(path: string): any {
+        let data = this.readFile(path);
+        return (data == undefined) ? undefined : ts.ScriptSnapshot.fromString(data);
+    }
+
+    readFile(path: string): string {
+        let file = this.files.get(path);
+        return (file == undefined) ? undefined : file.contents;
+    }
 
     getCurrentDirectory(): string {
         return "";
     }
 
-    getExecutingFilePath(): string {
-        return "";
+    getCompilationSettings(): any {
+        return compilationOptions;
     }
 
-    fileExists(path): boolean { return this.files[path] != undefined; }
+    getDefaultLibFileName(options: any): string {
+        return "lib.ts.d";
+    }
 
-    readFile(path: string, encoding: string): string {
-        console.log(`looking for ${path}`);
-        let file = this.files[path];
-        if (file == undefined) {
-            return undefined;
-        }
+    fileExists(path: string): boolean {
+        return this.files.has(path);
+    }
 
-        return file.contents;
+    readDirectory(path: string) {
+        throw "readDirectory not supported";
     }
 
     writeFile(path: string, contents: string) {
         console.log(`writing ${path}`);
 
-        let file = this.files[path];
+        let file = this.files.get(path);
         if (file == undefined) {
             file = new VirtualFile();
             file.name = path;
-            this.files[path] = file;
+            this.files.set(path, file);
+            this.projectVersion++;
         }
 
         file.contents = contents;
-        file.watchers.forEach((watcher) => {
-            watcher.callback(path, FileWatcherEventKind.Changed)
-        });
+        file.version++;
+        this.projectVersion++;
     }
 
-    directoryExists(path: string): boolean {
-        return false;
-    }
-
-    watchFile(path: string, callback, defaultPollingInterval): FileWatcher {
-        console.log(`watching ${path}`);
-        let file = this.files[path];
-        if (file == undefined) {
-            return undefined;
-        }
-
-        let watcher = new FileWatcher();
-        watcher.file = file;
-        watcher.callback = callback;
-        file.watchers.add(watcher);
-        return watcher;
-    }
-
-    setTimeout?(callback: (...args: any[]) => void, ms: number, ...args: any[]): any {
-        return window.setTimeout(callback, ms, args);
-    }
-
-    clearTimeout?(timeoutId: any): void {
-        window.clearTimeout(timeoutId);
-    }
-
-    getDefaultLibFileName(): string { return "lib.d.ts"; }
-    useCaseSensitiveFileNames() { return false; }
-    getCanonicalFileName(fileName) { return fileName; }
-    getDirectories() { return []; }
-
-    newLine = "\n";
+    trace = console.log;
 };
 
-(function(){
-    let scripts = document.getElementsByTagName("SCRIPT");
-    for (let i=0; i<scripts.length; i++) {
-        let script = scripts[i];
-        if (script.getAttribute("type") == "typescript-lib") {
-            let leafName: string = (script as any).leafName;
-            if (leafName.startsWith("lib.")) {
-                sys.writeFile(leafName, (script as any).text;
-            }
-        }
-    }
-})();
+let languageServices = ts.createLanguageService(
+    languageServiceHost,
+    ts.createDocumentRegistry()
+);
 
-
-sys.writeFile("fnord.ts", `
-class Fnord {
-    floo() {
-        console.log(7);
-    }
+languageServiceHost.writeFile("blah.ts", `
+class Bling {
 }
 `);
-
-function reportDiagnostic(diagnostic: any) {
-    console.log(`diagnostic: ${diagnostic.messageText}`);
-}
-
-function reportWatchStatusChanged(message: string) {
-    console.log(`watch status changed: ${JSON.stringify(message)}`);
-}
-
-const host = ts.createWatchCompilerHost(
-    ["fnord.ts"],
-    {
-        target: ts.ScriptTarget.ES5,
-    	strict: true,
-        suppressOutputPathCheck: false,
-        extendedDiagnostics: true,
-    },
-    sys,
-    ts.createEmitAndSemanticDiagnosticsBuilderProgram,
-    reportDiagnostic,
-    reportWatchStatusChanged
-);
-host.writeFile = function(path: string, value: string) {
-    console.log(`writing result program ${path} = ${value}`);
-}
-host.trace = console.log
-
-ts.createWatchProgram(host);
-
-sys.writeFile("fnord.ts", `
+languageServiceHost.writeFile("fnord.ts", `
 class Fnord {
     floo() {
         console.log(7);
@@ -163,3 +106,23 @@ class Blah extends Fnord {
 }
 
 `);
+
+function emitAll() {
+    for (let tsf of languageServiceHost.getScriptFileNames()) {
+        let output = languageServices.getEmitOutput(tsf);
+        if (!output.emitSkipped) {
+            for (let f of output.outputFiles) {
+                console.log(`name: ${f.name}`);
+                console.log(f.text);
+            }
+        }
+    }
+}
+emitAll();
+
+languageServiceHost.writeFile("blah.ts", `
+class Bling {
+    mnmnmn() {}
+}
+`);
+emitAll();
