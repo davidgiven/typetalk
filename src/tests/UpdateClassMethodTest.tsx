36af74f3e92efb50fe8da785f28bbf71b3ef439d
David Given <dg@cowlark.com>
2019-03-20 22:24:27 +0100

    Added some more tests. It *actually seems to be working*.

---
+++ src/tests/UpdateClassMethodTest.tsx
@@ -0,0 +1,24 @@
+class UpdateClassMethodTest extends AbstractTest {
+    run() {
+        classRegistry.set("TestClass", null,
+        `
+            class TestClass {
+                m(): number { return 1; }
+            };
+        `);
+
+        classRegistry.recompile();
+        let o = new globals.TestClass();
+        this.assert(o.m() == 1, "method did not fire");
+
+        classRegistry.set("TestClass", null,
+        `
+            class TestClass {
+                m(): number { return 2; }
+            };
+        `);
+        classRegistry.recompile();
+
+        this.assert(o.m() == 2, "updated method did not fire");
+    }
+}
---
+++ src/tests/UpdateSuperclassMethodTest.tsx
@@ -0,0 +1,30 @@
+class UpdateSuperclassMethodTest extends AbstractTest {
+    run() {
+        classRegistry.set("TestClass", null,
+        `
+            class TestClass {
+                m(): number { return 1; }
+            };
+        `);
+
+        classRegistry.set("TestSubclass", null,
+        `
+            class TestSubclass extends TestClass {
+            };
+        `);
+
+        classRegistry.recompile();
+        let o = new globals.TestSubclass();
+        this.assert(o.m() == 1, "superclass method did not fire");
+
+        classRegistry.set("TestClass", null,
+        `
+            class TestClass {
+                m(): number { return 2; }
+            };
+        `);
+        classRegistry.recompile();
+
+        this.assert(o.m() == 2, "updated superclass method did not fire");
+    }
+}
